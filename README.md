b178e3b562cd3c6891745d7273158ef42ed9ac2e
arnavjainn06 <jainarnav@outlook.com>
2021-06-03 16:42:16 +0400

    added flag choice functionality

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # PyMix Filter - Just For Fun
  
- > This Python App Is Under Development
+## Did you ever want to find out what footballer you look like.... Well Now You Can!
+### Here's How 👇
+ 
 
 ### App Maintained And Developed By Arnav Jain
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import cv2
 import numpy as np
 import face_recognition
 import os
+import time
 
+flag = ["Celebrities", "Celebrity"]
 
 def null():
     print("nullified")
@@ -11,30 +13,30 @@ def null():
 # Declaring the variable for {x} digit for the file to be saved
 token = 0
 
-# Loading Footballers
-asset_01 = face_recognition.load_image_file("assets/Footballers/asset_01.jpg")
-asset_02 = face_recognition.load_image_file("assets/Footballers/asset_02.jpg")
-asset_03 = face_recognition.load_image_file("assets/Footballers/asset_03.jpg")
-asset_04 = face_recognition.load_image_file("assets/Footballers/asset_04.jpg")
-asset_05 = face_recognition.load_image_file("assets/Footballers/asset_05.jpg")
-asset_06 = face_recognition.load_image_file("assets/Footballers/asset_06.jpg")
-asset_07 = face_recognition.load_image_file("assets/Footballers/asset_07.jpg")
-asset_08 = face_recognition.load_image_file("assets/Footballers/asset_08.jpg")
-asset_09 = face_recognition.load_image_file("assets/Footballers/asset_09.jpg")
-asset_10 = face_recognition.load_image_file("assets/Footballers/asset_10.jpg")
+# Loading Assets
+asset_01 = face_recognition.load_image_file(f"assets/{flag[0]}/asset_01.jpg")
+asset_02 = face_recognition.load_image_file(f"assets/{flag[0]}/asset_02.jpg")
+asset_03 = face_recognition.load_image_file(f"assets/{flag[0]}/asset_03.jpg")
+asset_04 = face_recognition.load_image_file(f"assets/{flag[0]}/asset_04.jpg")
+asset_05 = face_recognition.load_image_file(f"assets/{flag[0]}/asset_05.jpg")
+asset_06 = face_recognition.load_image_file(f"assets/{flag[0]}/asset_06.jpg")
+asset_07 = face_recognition.load_image_file(f"assets/{flag[0]}/asset_07.jpg")
+asset_08 = face_recognition.load_image_file(f"assets/{flag[0]}/asset_08.jpg")
+asset_09 = face_recognition.load_image_file(f"assets/{flag[0]}/asset_09.jpg")
+asset_10 = face_recognition.load_image_file(f"assets/{flag[0]}/asset_10.jpg")
 
-# Pushing the football assets into a list
-footballAssets = [asset_01, asset_02, asset_03, asset_04, asset_05, asset_06, asset_07, asset_08, asset_09, asset_10]
-coloredFootballAssets = []
+# Pushing the assets into a list
+mainAssets = [asset_01, asset_02, asset_03, asset_04, asset_05, asset_06, asset_07, asset_08, asset_09, asset_10]
+coloredAssets = []
 
 # Map through the list to make every image RGB
-for x in range(len(footballAssets)):
-    footballAssets[x] = cv2.cvtColor(footballAssets[x], cv2.COLOR_BGR2RGB)
-    footbalObj = {
-        "img": footballAssets[x],
-        "faceLocation": face_recognition.face_locations(footballAssets[x])[0]
+for x in range(len(mainAssets)):
+    mainAssets[x] = cv2.cvtColor(mainAssets[x], cv2.COLOR_BGR2RGB)
+    assetObj = {
+        "img": mainAssets[x],
+        "faceLocation": face_recognition.face_locations(mainAssets[x])[0]
     }
-    coloredFootballAssets.append(footbalObj)
+    coloredAssets.append(assetObj)
 
 
 cap = cv2.VideoCapture(0)
@@ -47,41 +49,43 @@ while True:
     # Display the resulting frame
     cv2.putText(camera, "Press Space To Capture", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
     cv2.putText(camera, "Press Esc To Exit", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-
-    k = cv2.waitKey(1)
-
     cv2.imshow('Camera', camera)
+    k = cv2.waitKey(1)
 
     # If spacebar is pressed 👇
     if k%256 == 32:
-        imageName = f"pymix-filter-{token}.png"
-        cv2.imwrite(imageName, camera)
-        cv2.putText(camera, "Loading...", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        try:
+            imageName = f"pymix-filter-{token}.png"
+            cv2.imwrite(imageName, camera)
+            cv2.putText(camera, "Loading...", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            scores = []
 
-        scores = []
-        # Code to compare faces
-        for x in range(len(coloredFootballAssets)): # Map through every asset to compare with snapshot
-            snapshot = cv2.imread("pymix-filter-0.png")
-            # -------------------------------------------------------------------------
-            encodedSnapshot = face_recognition.face_encodings(snapshot)[0]
-            encodedAsset = face_recognition.face_encodings(coloredFootballAssets[x]["img"])
-            # -------------------------------------------------------------------------
-            results = face_recognition.compare_faces(encodedSnapshot, encodedAsset)
-            faceDistance = face_recognition.face_distance(encodedSnapshot, encodedAsset)
+            # Code to compare faces
+            for x in range(len(coloredAssets)):  # Map through every asset to compare with snapshot
+                snapshot = cv2.imread("pymix-filter-0.png")
+                # -------------------------------------------------------------------------
+                encodedSnapshot = face_recognition.face_encodings(snapshot)[0]
+                encodedAsset = face_recognition.face_encodings(coloredAssets[x]["img"])
+                # -------------------------------------------------------------------------
+                results = face_recognition.compare_faces(encodedSnapshot, encodedAsset)
+                faceDistance = face_recognition.face_distance(encodedSnapshot, encodedAsset)
+                # print(results, faceDistance)
 
-            # Rounding the face distances
-            roundedFaceDistance = round(faceDistance[0], 3)
-            # -------------------------------------------------------------------------
-            scores.append(roundedFaceDistance)
-            print("...")
+                # Rounding the face distances
+                roundedFaceDistance = round(faceDistance[0], 3)
+                # -------------------------------------------------------------------------
+                scores.append(roundedFaceDistance)
+                print("...")
 
-        print(scores)
-        lowestScore = min(scores)
-        lowestScoreIndex = scores.index(lowestScore)
-        footballMatch = coloredFootballAssets[lowestScoreIndex]["img"]
-        cv2.putText(footballMatch, "We Found Your Match!", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.imshow("Match", footballMatch)
-        cv2.waitKey(0)
+            lowestScore = min(scores)
+            lowestScoreIndex = scores.index(lowestScore)
+            assetMatch = coloredAssets[lowestScoreIndex]["img"]
+            cv2.putText(assetMatch, f"We Found Your {flag[1]} Look Alike!", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.imshow("Match", assetMatch)
+            cv2.waitKey(0)
+
+        except Exception as e:
+            cv2.putText(camera, "Please Try And Show Your Entire Face Properly", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
 
     elif k%256 == 27:
         os.remove("pymix-filter-0.png")
